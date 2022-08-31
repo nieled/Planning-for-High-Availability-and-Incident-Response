9703a9b59504a84becae7ca2cff6b7474d135976
Daniel Calle <nieled@riseup.net>
2022-08-30 20:42:09 -0500

    update config

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -8,7 +8,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.28"
+      configuration_aliases = [
+        aws.usw1
+      ]
     }
   }
 }
