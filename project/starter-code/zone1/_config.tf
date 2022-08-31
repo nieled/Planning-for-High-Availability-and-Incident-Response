15197b549e6513e29dae3729bc1072189159b9c1
Daniel Calle <nieled@riseup.net>
2022-08-30 20:36:27 -0500

    add providers

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -1,21 +1,39 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-tf-nieled"
-     key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
-   }
+  backend "s3" {
+    bucket = "udacity-tf-nieled"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
 
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
- provider "aws" {
-   region = "us-east-2"
+provider "aws" {
+  region = "us-east-2"
 
-   default_tags {
-     tags = local.tags
-   }
- }
+  default_tags {
+    tags = local.tags
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
- provider "aws" {
+provider "aws" {
   alias  = "usw1"
   region = "us-west-1"
+}
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 3.0"
+  }
 }
